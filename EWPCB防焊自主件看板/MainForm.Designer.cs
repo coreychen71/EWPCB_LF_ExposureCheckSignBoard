e962a495d629fc8670dea00b51f86cef9ee3f775
coreychen71 <coreychen71@gmail.com>
2016-07-14 14:51:24 +0800

    加入專案檔案。

---
+++ EWPCB防焊自主件看板/MainForm.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace EWPCB防焊自主件看板
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.dgvData = new System.Windows.Forms.DataGridView();
+            this.tmrRefresh = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvData
+            // 
+            this.dgvData.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("微軟正黑體", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvData.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            this.dgvData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("微軟正黑體", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvData.DefaultCellStyle = dataGridViewCellStyle4;
+            this.dgvData.Location = new System.Drawing.Point(3, 2);
+            this.dgvData.Name = "dgvData";
+            this.dgvData.RowHeadersVisible = false;
+            this.dgvData.RowTemplate.Height = 24;
+            this.dgvData.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvData.Size = new System.Drawing.Size(779, 557);
+            this.dgvData.TabIndex = 0;
+            // 
+            // tmrRefresh
+            // 
+            this.tmrRefresh.Tick += new System.EventHandler(this.tmrRefresh_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.dgvData);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "防焊曝光自主件檢驗看板";
+            this.TopMost = true;
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.dgvData)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvData;
+        private System.Windows.Forms.Timer tmrRefresh;
+    }
+}
+
---
+++ EWPCB防焊自主件看板/MainForm.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace EWPCB防焊自主件看板
+{
+    public partial class MainForm : Form
+    {
+        string strCon = "server=EWNAS;database=ME;uid=me;pwd=2dae5na";
+        string strComm = "";
+        DataTable srcData = new DataTable();
+        DateTime date = DateTime.Now;
+        DateTime updateTime = DateTime.Now;
+        int clock = 180;
+        public MainForm()
+        {
+            InitializeComponent();
+            dgvData.ReadOnly = true;
+            DataRefresh(date.ToString("yyyy-MM-dd 00:00:00"));
+            Text = "防焊曝光自主件檢驗看板(" + clock + ") - " + updateTime.ToString("yyyy-MM-dd HH:mm:ss");
+            tmrRefresh.Interval = 1000;
+            tmrRefresh.Start();
+        }
+
+        private void DataRefresh(string date)
+        {
+            strComm = "select partnum as '料號',machineno as '機台號',workqnty as '檢修數',qcresult as '結果'," +
+                "qcman as '檢驗人',CONVERT(char(19), starttime, 120) as '放板時間',CONVERT(char(19),endtime,120) " +
+                "as '結束時間' from drymcse where departname = 'LF' and process = '自主件' and todo = 1 and " +
+                "starttime >='" + date + "' order by starttime desc";
+            using (SqlConnection sqlcon = new SqlConnection(strCon))
+            {
+                using (SqlCommand sqlcomm = new SqlCommand(strComm, sqlcon))
+                {
+                    try
+                    {
+                        sqlcon.Open();
+                        SqlDataReader read = sqlcomm.ExecuteReader();
+                        srcData.Load(read);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+            }
+            dgvData.DataSource = srcData;
+            dgvData.Columns["料號"].Width = 200;
+            dgvData.Columns["機台號"].Width = 100;
+            dgvData.Columns["檢修數"].Width = 100;
+            dgvData.Columns["結果"].Width = 335;
+            dgvData.Columns["檢驗人"].Width = 100;
+            dgvData.Columns["放板時間"].Width = 250;
+            dgvData.Columns["結束時間"].Width = 250;
+            dgvData.DataBindingComplete += ChangRowColor;
+        }
+
+        //在資料繫結後，所觸發的事件
+        private void ChangRowColor(object sender, DataGridViewBindingCompleteEventArgs e)
+        {
+            foreach (DataGridViewRow row in dgvData.Rows)
+            {
+                if (Convert.ToString(row.Cells["結果"].Value).Trim().ToUpper() == "OK")
+                {
+                    row.DefaultCellStyle.BackColor = Color.Lime;
+                    row.DefaultCellStyle.SelectionBackColor = Color.Lime;
+                    row.DefaultCellStyle.SelectionForeColor = Color.Black;
+                }
+                else if (Convert.ToString(row.Cells["結果"].Value).Trim().ToUpper() != "")
+                {
+                    row.DefaultCellStyle.BackColor = Color.LightSalmon;
+                    row.DefaultCellStyle.SelectionBackColor = Color.LightSalmon;
+                    row.DefaultCellStyle.SelectionForeColor = Color.Black;
+                }
+            }
+        }
+
+        private void tmrRefresh_Tick(object sender, EventArgs e)
+        {
+            clock--;
+            Text = "防焊曝光自主件檢驗看板(" + clock + ") - " + updateTime.ToString("yyyy-MM-dd HH:mm:ss");
+            if (clock == 0)
+            {
+                date = DateTime.Now;
+                updateTime = DateTime.Now;
+                DataRefresh(date.ToString("yyyy-MM-dd 00:00:00"));
+                clock = 180;
+                Text = "防焊曝光自主件檢驗看板(" + clock + ") - " + updateTime.ToString("yyyy-MM-dd HH:mm:ss");
+            }
+        }
+    }
+}
